301d7ebbabec4cedf9d21767bf6b4f65b135e2cc
Andrew James <andrewpjames87@gmail.com>
2015-02-04 15:50:57 -0700

    Users can update users and can delete useres from the edit screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -6,6 +7,24 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @submit_name = "Create User"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @submit_name = "Update User"
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
@@ -22,7 +41,17 @@ class UsersController < ApplicationController
       end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, notice: "User was successfully deleted."
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
