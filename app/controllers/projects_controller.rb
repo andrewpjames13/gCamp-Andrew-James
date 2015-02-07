3ba68deb31fc07ad8dbd31d72dfc998d7a423f57
Andrew James <andrewpjames87@gmail.com>
2015-02-07 11:03:32 -0700

    Added delete functionality and made it so the projects can be udated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,8 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project = Project.find(params[:id])
+    @submit_name = "Update Project"
   end
 
   def show
@@ -34,6 +36,17 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    @project = Project.find(params[:id])
+    
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
