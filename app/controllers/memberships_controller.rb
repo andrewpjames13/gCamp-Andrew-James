eeb9287f9949833ea477644bc92c364124594393
Andrew James <andrewpjames87@gmail.com>
2015-03-09 15:00:51 -0600

    Add user collection select to membership index

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,6 +4,13 @@ class MembershipsController < ApplicationController
     @memberships = Membership.all
     @project = Project.find(params[:project_id])
     @users = User.all
+    @membership = @project.memberships.new
+  end
+
+  private
+
+  def membership_params
+    params.require(:membership).permit(:user_id, :project_id, :role)
   end
 
 end
