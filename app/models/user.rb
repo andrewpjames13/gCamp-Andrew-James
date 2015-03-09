7705f48485cc1e2f3003f25b29e542d039ae56e7
Andrew James <andrewpjames87@gmail.com>
2015-03-09 14:03:06 -0600

    Add memberships to database, created model, view, and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+
   def full_name
     "#{first_name} #{last_name}"
   end
+
+
+  has_many :memberships
+  has_many :projects, through: :memberships
+
   validates :first_name, :last_name, :password, presence: true
   validates :email, presence: true, uniqueness: :true
 
