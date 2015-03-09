532a1de1ae9b274beb5f37a224fb57c6e364cd59
Andrew James <andrewpjames87@gmail.com>
2015-03-09 14:35:22 -0600

    Add bread crumb to membership show

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,6 +3,7 @@ class MembershipsController < ApplicationController
   def index
     @memberships = Membership.all
     @project = Project.find(params[:project_id])
+    @users = User.all
   end
 
 end
