117dc96b865deb6f30e8e4ab876e31e581ceb9ee
Andrew James <andrewpjames87@gmail.com>
2015-03-12 20:26:49 -0600

    Add validations for memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,7 @@
 class Membership < ActiveRecord::Base
 
+  validates :user_id, presence: :true, uniqueness: {scope: :project_id, message: "has already been added to this project."}
+
   belongs_to :user
   belongs_to :project
 
