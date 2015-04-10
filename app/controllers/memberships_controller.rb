87948304a77676156d9b21b7b80534145ffff601
Andrew James <andrewpjames87@gmail.com>
2015-04-10 10:47:39 -0600

    Member can remove them selves from project

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -30,7 +30,11 @@ class MembershipsController < ApplicationController
     @project = Project.find(params[:project_id])
     @membership = Membership.find(params[:id])
     @membership.destroy
-    redirect_to project_memberships_path(@project), notice: "Member was successfully deleated"
+    if params[:from]=='own_membership'
+      redirect_to projects_path, notice: "#{@membership.user.full_name} was successfully removed"
+    else
+      redirect_to project_memberships_path(@project), notice: "Member was successfully deleated"
+    end
   end
 
   private
