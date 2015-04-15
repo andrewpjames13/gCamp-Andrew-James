aeaa5ab30699ee4f3049c48d09c922b7b8c58d70
Andrew James <andrewpjames87@gmail.com>
2015-04-15 16:56:22 -0600

    Project capybarra specs updated

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -4,7 +4,8 @@ describe 'User can CRUD new project' do
 
   before :each do
     visit "/"
-    User.create(:first_name => "TestFirstName", :last_name => 'TestLastName', :email => 'Test@email.com', :password => 'mypass', :password_confirmation => 'mypass')
+    @user = User.create(:first_name => "TestFirstName", :last_name => 'TestLastName', :email => 'Test@email.com', :password => 'mypass', :password_confirmation => 'mypass')
+
     click_on "Sign In"
     expect(page).to have_content("Sign into gCamp")
 
@@ -13,12 +14,15 @@ describe 'User can CRUD new project' do
     click_button "Sign In"
 
     expect(page).to have_content("TestFirstName")
-    click_on "Projects"
+
   end
 
   scenario "User can create a new Project" do
 
-    click_on "New Project"
+    within(".pull-right") do
+      click_link('New Project')
+    end
+
     expect(page).to have_content("Create Project")
 
     fill_in 'project[name]', :with => "TestName"
@@ -29,36 +33,74 @@ describe 'User can CRUD new project' do
   end
 
   scenario "User can view project show page" do
-    Project.create(:name => "TestName")
+    within(".pull-right") do
+      click_link('New Project')
+    end
 
-    visit "projects"
+    expect(page).to have_content("Create Project")
 
-    click_on "TestName"
+    fill_in 'project[name]', :with => "TestName"
+    click_on "Create Project"
+    expect(page).to have_content("Project was successfully created!")
+    expect(page).to have_content("TestName")
+
+    within(".breadcrumb") do
+      click_link('TestName')
+    end
+
     expect(page).to have_content("TestName")
 
   end
 
   scenario "User can edit a project" do
-    Project.create(:name => "TestName")
+    within(".pull-right") do
+      click_link('New Project')
+    end
 
-    visit "projects"
+    expect(page).to have_content("Create Project")
 
-    click_on "Edit"
-    expect(page).to have_content("Edit Project")
+    fill_in 'project[name]', :with => "TestName"
+    click_on "Create Project"
+    expect(page).to have_content("Project was successfully created!")
+    expect(page).to have_content("TestName")
 
-    fill_in 'project[name]', :with => "TestEdit"
-    click_on "Update Project"
-    expect(page).to have_content("Project was successfully updated.")
-    expect(page).to have_content("TestEdit")
+    within(".breadcrumb") do
+      click_link('TestName')
+    end
+
+    expect(page).to have_content("TestName")
+
+
+    within(".pull-right") do
+      click_link('Edit')
+    end
+
+
+    expect(page).to have_content("TestName")
   end
 
   scenario "User can delete a project" do
-    Project.create(:name => "TestName")
+    within(".pull-right") do
+      click_link('New Project')
+    end
 
-    visit "projects"
+    expect(page).to have_content("Create Project")
 
-    click_on "Delete"
-    expect(page).to have_content("Project was successfully destroyed.")
+    fill_in 'project[name]', :with => "TestName"
+    click_on "Create Project"
+    expect(page).to have_content("Project was successfully created!")
+    expect(page).to have_content("TestName")
+
+    within(".breadcrumb") do
+      click_link('TestName')
+    end
+
+    expect(page).to have_content("TestName")
+
+
+    within(".well") do
+      click_link('Delete')
+    end
   end
 
 end
